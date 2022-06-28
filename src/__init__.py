97792f16626b937d93ecf56faa79c2863f6aedef
Felix Korovin <felix.korovin@icloud.com>
2022-06-28 15:34:24 +0300

    Git ignore updated, .idead dir removed

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,1 @@
-from tiny_emitter import TinyEmitter, set_log_level
+from src.tiny_emitter import TinyEmitter, set_log_level
--- tests/test_tiny_emitter.py
+++ tests/test_tiny_emitter.py
@@ -1,4 +1,13 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import unittest
+import src.tiny_emitter
 
+
+# TODO: implement
+
+
+class TestSum(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
