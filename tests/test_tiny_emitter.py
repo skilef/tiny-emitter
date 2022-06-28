24616fe8a619fd4268f4563082910f1062345c3d
Felix Korovin <felix.korovin@icloud.com>
2022-06-28 15:59:49 +0300

    Example test stub

--- tests/test_tiny_emitter.py
+++ tests/test_tiny_emitter.py
@@ -4,8 +4,10 @@ import unittest
 import tiny_emitter
 
 
-class TestSum(unittest.TestCase):
-    pass
+class TestTinyEmitter(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
